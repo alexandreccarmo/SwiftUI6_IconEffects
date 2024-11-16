0ef336630e9604b35974464b2c29571e40dc4dc8
Alexandre Carmo <alecarmo1980@gmail.com>
2024-11-16 15:08:59 -0300

    Initial Commit

---
+++ IconEffects_Tests/IconEffects_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  IconEffects_Tests.swift
+//  IconEffects_Tests
+//
+//  Created by Alexandre C do Carmo on 16/11/24.
+//
+
+import Testing
+@testable import IconEffects_
+
+struct IconEffects_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
